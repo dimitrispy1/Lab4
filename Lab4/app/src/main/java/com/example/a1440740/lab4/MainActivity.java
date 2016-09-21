ad61f876c7fcc027d6aa8f8161acf60501763e0f
Dimitri Spy <dimitri.spyropoulos@dawsoncollege.qc.ca>
2016-09-21 17:24:30 -0400

    Lab4

---
+++ Lab4/app/src/main/java/com/example/a1440740/lab4/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.a1440740.lab4;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void showMap(String s) {
+        Uri geoLocation = Uri.parse(("geo:0,0?q=") + Uri.encode(s));
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(geoLocation);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
+    public void onClick(View v){
+        showMap(getString(R.string.country));
+    }
+}
